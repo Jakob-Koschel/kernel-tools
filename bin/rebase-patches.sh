c22fa4e62c4fc9b0d56b6a3896920cc9f4fbcdf8
Jakob Koschel <jakobkoschel@gmail.com>
2022-04-02 11:51:22 +0200

    rebase-patches.sh: allow specifying rebase branch

--- bin/rebase-patches.sh
+++ bin/rebase-patches.sh
@@ -4,10 +4,15 @@ set -e
 
 # pass in directory with .patch files (use at own caution)
 PATCHES=$1
+REBASE_BRANCH=$2
+
+if [ -z $REBASE_BRANCH ]; then
+  REBASE_BRANCH=rebase
+fi
 
 git checkout master
-git branch -D rebase
-git checkout -b rebase
+git branch -D $REBASE_BRANCH
+git checkout -b $REBASE_BRANCH
 
 git am $PATCHES/*.patch
 mv $PATCHES $PATCHES.bak
