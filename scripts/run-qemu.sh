9e9a1ea71694259cf5ae99397e932f8480c442b0
Jakob Koschel <jkl820.git@gmail.com>
2022-08-02 18:02:42 +0200

    run-qemu: add QEMU_CPU variable

--- scripts/run-qemu.sh
+++ scripts/run-qemu.sh
@@ -4,6 +4,7 @@ KERNEL="-kernel ${KERNEL}/arch/x86/boot/bzImage"
 APPEND="console=ttyS0 nokaslr nosmp maxcpus=1 rcu_nocbs=0 nmi_watchdog=0 ignore_loglevel modules=sd-mod,usb-storage,ext4 rootfstype=ext4 earlyprintk=serial"
 APPEND+=" biosdevname=0 kvm-intel.emulate_invalid_guest_state=1 kvm-intel.enable_apicv=1 kvm-intel.enable_shadow_vmcs=1 kvm-intel.ept=1 kvm-intel.eptad=1 kvm-intel.fasteoi=1 kvm-intel.flexpriority=1 kvm-intel.nested=1 kvm-intel.pml=1 kvm-intel.unrestricted_guest=1 kvm-intel.vmm_exclusive=1 kvm-intel.vpid=1 net.ifnames=0"
 MEMORY="8192"
+QEMU_CPU="${QEMU_CPU:=qemu64,+smep,+smap}"
 
 TYPE=$1
 if [[ $TYPE = "syzkaller" ]]; then
@@ -35,10 +36,9 @@ qemu-system-x86_64 \
   ${VM_SHARED_FOLDER:+ -hdc fat:"${VM_SHARED_FOLDER}"} \
   -display none \
   -smp 1 \
-  -cpu qemu64,+smep,+smap \
+  -cpu ${QEMU_CPU} \
   -m ${MEMORY} \
   -echr 17 \
   -serial mon:stdio \
   -snapshot \
   -no-reboot
-
